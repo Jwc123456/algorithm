750a57042e1565016f31a2e74d16171125996fca
wwcc <1351786511@qq.com>
2025-02-25 20:52:00 +0800

    392

---
+++ src/main/java/com/wwcc/leetcode/Demo392.java
@@ -0,0 +1,72 @@
+package com.wwcc.leetcode;
+
+/**
+ * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+ *
+ * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+ *
+ * 进阶：
+ *
+ * 如果有大量输入的 S，称作 S1, S2, ... , Sk 其中 k >= 10亿，你需要依次检查它们是否为 T 的子序列。在这种情况下，你会怎样改变代码？
+ *
+ * 致谢：
+ *
+ * 特别感谢 @pbrother 添加此问题并且创建所有测试用例。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：s = "abc", t = "ahbgdc"
+ * 输出：true
+ * 示例 2：
+ *
+ * 输入：s = "axc", t = "ahbgdc"
+ * 输出：false
+ *
+ *
+ * 提示：
+ *
+ * 0 <= s.length <= 100
+ * 0 <= t.length <= 10^4
+ * 两个字符串都只由小写字符组成。
+ * @author wwcc
+ * @date 2025/02/25 20:46:42
+ */
+public class Demo392 {
+
+
+    public static boolean isSubsequence(String s, String t) {
+        String temp = t;
+        for(int i = 0;i < s.length();i++){
+            int index = temp.indexOf(s.charAt(i));
+            if(index == -1){
+                return false;
+            }
+            temp = temp.substring(index+1);
+        }
+        return true;
+    }
+
+    /**
+     * 双指针
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubsequence1(String s, String t) {
+        int n = s.length(), m = t.length();
+        int i = 0, j = 0;
+        while (i < n && j < m) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == n;
+    }
+
+    public static void main(String[] args) {
+        isSubsequence("bb", "ahbgdc");
+    }
+}
